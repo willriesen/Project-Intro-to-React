77a5f5d91b197550f93efafe7645beb7c157860a
Will Riesen <42421306+willriesen@users.noreply.github.com>
2019-02-15 10:08:43 -0600

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+ class ToDo extends Component {
+   render() {
+     return (
+       <li>
+             <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <span>{ this.props.description }</span>
+       </li>
+     );
+   }
+ }
+
+ export default ToDo;
